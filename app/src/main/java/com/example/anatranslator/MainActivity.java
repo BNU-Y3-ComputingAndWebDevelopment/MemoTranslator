c6beec08c0cd0f9b5ca78e114cf091eefd855d12
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-11-21 20:55:05 +0000

    Added block comment

--- app/src/main/java/com/example/anatranslator/MainActivity.java
+++ app/src/main/java/com/example/anatranslator/MainActivity.java
@@ -4,6 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * Institution: Buckinghamshire New University
+ * Academic Year: 2020/2021
+ * Module: Advanced Mobile Systems
+ * Author: Ana Lucia Petinga Zorro
+ *
+ * The MainActivity class specifies the features the user will be able to see once the
+ * AnaTranslator is launched.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
