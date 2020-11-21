f9aa4b647d805bdbc00c7dae420cffc837a66b04
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-11-21 19:20:36 +0000

    In TranslateViewModel.java set LruCache translators for file storage + created a new object of it.

--- app/src/main/java/com/example/anatranslator/TranslateViewModel.java
+++ app/src/main/java/com/example/anatranslator/TranslateViewModel.java
@@ -1,5 +1,19 @@
 package com.example.anatranslator;
 
-public class TranslateViewModel {
+import android.util.LruCache;
 
+import androidx.lifecycle.AndroidViewModel;
+
+import com.google.mlkit.common.model.RemoteModelManager;
+import com.google.mlkit.nl.translate.Translator;
+import com.google.mlkit.nl.translate.TranslatorOptions;
+
+public class TranslateViewModel extends AndroidViewModel {
+    private static final int NUM_TRANSLATORS = 3;
+
+    private final RemoteModelManager modelManager;
+    private final LruCache<TranslatorOptions, Translator> translators =
+            new LruCache<TranslatorOptions, Translator>(NUM_TRANSLATORS){
+
+            }
 }
