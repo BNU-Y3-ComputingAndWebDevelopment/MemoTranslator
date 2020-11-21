c99904a98618f603b2f541f598cb9a5c454c4e51
Ana Lucia P Zorro <alpz94@users.noreply.github.com>
2020-11-21 19:44:04 +0000

    In TranslateViewModel.java implemented a list of strings for when there are one or more sentences to translate.

--- app/src/main/java/com/example/anatranslator/TranslateViewModel.java
+++ app/src/main/java/com/example/anatranslator/TranslateViewModel.java
@@ -3,6 +3,7 @@ package com.example.anatranslator;
 import android.util.LruCache;
 
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.google.mlkit.common.model.RemoteModelManager;
@@ -10,6 +11,8 @@ import com.google.mlkit.nl.translate.TranslateLanguage;
 import com.google.mlkit.nl.translate.Translation;
 import com.google.mlkit.nl.translate.Translator;
 import com.google.mlkit.nl.translate.TranslatorOptions;
+
+import java.util.List;
 
 public class TranslateViewModel extends AndroidViewModel {
     private static final int NUM_TRANSLATORS = 3;
@@ -32,4 +35,8 @@ public class TranslateViewModel extends AndroidViewModel {
     MutableLiveData<TranslateLanguage.Language> sourceLang = new MutableLiveData<>();
     MutableLiveData<TranslateLanguage.Language> targetLang = new MutableLiveData<>();
     MutableLiveData<String> sourceText = new MutableLiveData<>();
+    MediatorLiveData<ResultOrError> translatedText = new MediatorLiveData<>();
+    //If there are a sentence or more to translate it will check for the available translation models
+    MutableLiveData<List<String>> availableModels =
+            new MutableLiveData<>();
 }
